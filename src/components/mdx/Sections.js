4ac081f0145ab1862962b7b6ae5cabe68841207b
Benedikt Rötsch <info@benedikt-roetsch.de>
2019-02-28 12:58:50 +0800

    improve scroll behaviour and remove scrollbar

--- src/components/mdx/Sections.js
+++ src/components/mdx/Sections.js
@@ -1,9 +1,15 @@
 import styled from 'styled-components'
 
 const Sections = styled.article`
-  scroll-snap-type: y mandatory;
+  scroll-snap-type: y proximity;
+  overflow-y: -moz-scrollbars-none;
+  -ms-overflow-style: none;
   overflow-y: scroll;
   height: 100vh;
+
+  &::-webkit-scrollbar {
+    width: 0 !important;
+  }
 `
 
 export default Sections
