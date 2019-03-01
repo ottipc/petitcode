0e9c2509b93dab16fc51f97d5e7b40e399dd519f
Benedikt Rötsch <info@benedikt-roetsch.de>
2019-03-01 12:26:37 +0800

    css hack to reduce chrome knockout text rendering issues by animating z-index to retrigger browser paint

--- src/components/mdx/KnockoutText.js
+++ src/components/mdx/KnockoutText.js
@@ -1,5 +1,6 @@
-import React from 'react'
-import styled from 'styled-components'
+import React, { useState } from 'react'
+import styled, { css } from 'styled-components'
+import Observer from '@researchgate/react-intersection-observer'
 
 const H1OutterWrapper = styled.div`
   background-color: #fff;
@@ -16,37 +17,71 @@ const VideoWrapper = styled.div`
   top: 0;
   left: 0;
   width: 100%;
+  height: 100%;
   z-index: 50;
+  background: #000;
+`
+
+const Video = styled.video`
+  display: block;
+  position: relative;
+  z-index: 51;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
 `
 
 const KnockoutH1 = styled.h1`
   position: relative;
-  z-index: 100;
   margin: 0;
-  padding: 0;
-  background-color: white;
+  padding: 0 0 1rem !important;
   color: black;
-  font-size: 10vmin;
-  mix-blend-mode: screen;
+  font-size: 12vmin;
   font-weight: bold;
+  z-index: 100;
+  background-color: white;
+  mix-blend-mode: screen;
+
+  /* css hack to get chrome render the knockout text properly */
+  transition: 0.5s z-index;
+  ${({ isVisible }) =>
+    isVisible &&
+    css`
+      z-index: 200;
+    `}
 `
 
 const KnockoutText = (props) => {
+  const [isVisible, setIsVisible] = useState(false)
+
+  const handleIntersection = (event) => {
+    const { isIntersecting } = event
+
+    if (isIntersecting) {
+      return setIsVisible(true)
+    }
+    setIsVisible(false)
+  }
+
   return (
-    <H1OutterWrapper>
-      <H1Wrapper>
-        <VideoWrapper>
-          <video autoPlay loop muted playsInline>
-            <source
-              src="//videos.ctfassets.net/pbrj6jtwg849/j7qCb94g9yMOi8MYIw40U/405d516b9e9cb3557906237cab5836be/SaaS_Video_3.mp4"
-              type="video/mp4"
-            />
-          </video>
-        </VideoWrapper>
-        <KnockoutH1 {...props} />
-      </H1Wrapper>
-    </H1OutterWrapper>
+    <Observer onChange={handleIntersection}>
+      <H1OutterWrapper>
+        <H1Wrapper>
+          <VideoWrapper>
+            {isVisible && (
+              <Video autoPlay loop muted playsInline>
+                <source
+                  src="//videos.ctfassets.net/pbrj6jtwg849/j7qCb94g9yMOi8MYIw40U/405d516b9e9cb3557906237cab5836be/SaaS_Video_3.mp4"
+                  type="video/mp4"
+                />
+              </Video>
+            )}
+          </VideoWrapper>
+          <KnockoutH1 {...props} isVisible={isVisible} />
+        </H1Wrapper>
+      </H1OutterWrapper>
+    </Observer>
   )
 }
 
-export default KnockoutText
+export default React.memo(KnockoutText)
