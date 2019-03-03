3d77fd0dbc740ca557a4019b80086ef896f1bc3a
Benedikt Rötsch <info@benedikt-roetsch.de>
2019-03-03 15:32:52 +0800

    change hamburger to animate to a cross

--- src/components/Overlays.js
+++ src/components/Overlays.js
@@ -8,20 +8,38 @@ import Navigation from './Navigation'
 import SectionNavigation from './SectionNavigation'
 import { NavigationContext, SectionContext } from '../utils/Contexts'
 
+const hamburgerLayerWidth = '35px'
+const hamburgerLayerHeight = '4px'
+const hamburgerLayerSpacing = '6px'
+const hamburgerPaddingX = '15px'
+const hamburgerPaddingY = '15px'
+const hamburgerActiveLayerColor = '#000000'
+const hamburgerLayerColor = '#ffffff'
+const hamburgerLayerBorderRadius = '3px'
+
+const hamburgerHoverOpacity = 0.7
+
 const Hamburger = styled.button`
   position: fixed;
   z-index: 10010;
   top: 50%;
   right: 0;
   transform: translateY(-50%);
-
+  padding: ${hamburgerPaddingY} ${hamburgerPaddingX};
   display: block;
   cursor: pointer;
-  transition-property: opacity, filter, color;
-  transition-duration: 0.3s;
+
+  transition-property: opacity, filter;
+  transition-duration: 0.15s;
   transition-timing-function: linear;
+
+  &:hover {
+    opacity: ${hamburgerHoverOpacity};
+  }
+
+  /* Normalize (<button>) */
   font: inherit;
-  color: ${({ colorScheme }) => colorScheme};
+  color: inherit;
   text-transform: none;
   background-color: transparent;
   border: 0;
@@ -31,63 +49,104 @@ const Hamburger = styled.button`
   &:focus {
     outline: none;
   }
-
-  ${({ navigationActive }) =>
-    navigationActive &&
-    css`
-      &:hover {
-        opacity: 0.7;
-      }
-      & > span > span,
-      & > span > span::before,
-      & > span > span::after {
-        background-color: #fff;
-      }
-      & > span > span::before {
-        transform: translate3d(8px, 0, 0) rotate(45deg) scale(0.7, 1);
-      }
-
-      & > span > span::after {
-        transform: translate3d(8px, 0, 0) rotate(-45deg) scale(0.7, 1);
-      }
-    `};
 `
 
 const HamburgerBox = styled.span`
-  width: 30px;
-  height: 20px;
-  display: block;
+  width: ${hamburgerLayerWidth};
+  height: calc(${hamburgerLayerHeight} * 3 + ${hamburgerLayerSpacing} * 2);
+  display: inline-block;
   position: relative;
+  /* 3dx */
+  perspective: calc(${hamburgerLayerWidth} * 2);
 `
 
 const HamburgerInner = styled.span`
   display: block;
   top: 50%;
-  margin-top: -2px;
+  margin-top: calc(${hamburgerLayerHeight} / -2);
+
   &,
   &::before,
   &::after {
-    width: 30px;
-    height: 3px;
-    background-color: ${({ colorScheme }) => colorScheme};
-    transition: background-color 0.3s linear;
-    border-radius: 4px;
+    width: ${hamburgerLayerWidth};
+    height: ${hamburgerLayerHeight};
+    background-color: ${hamburgerLayerColor};
+    border-radius: ${hamburgerLayerBorderRadius};
     position: absolute;
     transition-property: transform;
     transition-duration: 0.15s;
     transition-timing-function: ease;
+    box-sizing: content-box;
   }
+
   &::before,
   &::after {
     content: '';
     display: block;
   }
+
   &::before {
-    top: -7px;
+    top: calc(
+      (
+          ${hamburgerLayerSpacing} + ${hamburgerLayerHeight} + 2px
+            /* border size */
+        ) * -1
+    );
   }
+
   &::after {
-    bottom: -7px;
+    bottom: calc(
+      (
+          ${hamburgerLayerSpacing} + ${hamburgerLayerHeight} + 2px
+            /* border size */
+        ) * -1
+    );
   }
+
+  ${({ navigationActive }) =>
+    navigationActive &&
+    css`
+      background-color: ${hamburgerActiveLayerColor};
+    `}
+
+  /* 3dx */
+  transition: transform .15s cubic-bezier(.645,.045,.355,1),
+              background-color 0s cubic-bezier(.645,.045,.355,1) .1s;
+  &::before,
+  &::after {
+    transition: transform 0s cubic-bezier(0.645, 0.045, 0.355, 1) 0.1s;
+  }
+
+  ${({ navigationActive }) =>
+    navigationActive &&
+    css`
+      background-color: transparent !important;
+      transform: rotateY(180deg);
+
+      &::before,
+      &::after {
+        background: black !important;
+        border: 2px solid white;
+      }
+
+      &::before {
+        transform: translate3d(
+            0,
+            calc(${hamburgerLayerHeight} + ${hamburgerLayerSpacing}),
+            0
+          )
+          rotate(45deg);
+      }
+
+      &::after {
+        transform: translate3d(
+            0,
+            calc((${hamburgerLayerHeight} + ${hamburgerLayerSpacing}) * -1),
+            0
+          )
+          rotate(-45deg);
+      }
+    `}
 `
 
 const HeaderWrapper = styled.div`
@@ -119,12 +178,14 @@ export default function Overlays() {
       <Navigation navigationActive={navigationActive} />
       <Hamburger
         onClick={toggleNavigation}
-        navigationActive={navigationActive}
         aria-label="toggle menu"
         colorScheme={colorScheme}
       >
         <HamburgerBox>
-          <HamburgerInner colorScheme={colorScheme} />
+          <HamburgerInner
+            colorScheme={colorScheme}
+            navigationActive={navigationActive}
+          />
         </HamburgerBox>
       </Hamburger>
       <SectionNavigation colorScheme={colorScheme} />
