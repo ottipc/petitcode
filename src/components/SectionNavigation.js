dde42b58dac7be9e994e05d1ece534009115b793
Benedikt Rötsch <info@benedikt-roetsch.de>
2019-04-05 14:39:02 +0800

    bigger touch area for section navigation and use css filter to make visible on images

--- src/components/SectionNavigation.js
+++ src/components/SectionNavigation.js
@@ -7,35 +7,48 @@ const Wrapper = styled.div`
   position: fixed;
   z-index: 900;
   top: 50%;
-  left: ${({ theme }) => theme.spacings.s1};
+  left: 0;
   transform: translateY(-50%);
   display: flex;
   flex-direction: column;
+  mix-blend-mode: difference;
 `
 
 const Button = styled.button`
+  box-sizing: content-box;
+  position: relative;
   display: block;
   width: 4px;
   height: 21px;
   margin-bottom: 6px;
-  padding: 0;
+  padding: 0 ${({ theme }) => theme.spacings.s1};
   border: 0;
   overflow: hidden;
   color: transparent;
-  transition-property: opacity, color, height;
-  transition-duration: 0.3s;
-  background: ${({ colorScheme }) => colorScheme};
+  transition: 0.3s opacity linear;
+  background: transparent;
   cursor: pointer;
 
   &:last-child {
     margin-bottom: 0;
   }
 
+  &:after {
+    content: '';
+    display: block;
+    background: ${({ theme }) => theme.colors.white};
+    width: 4px;
+    position: absolute;
+    left: ${({ theme }) => theme.spacings.s1};
+    top: 0;
+    bottom: 0;
+  }
+
   opacity: 0;
   ${({ show }) =>
     show &&
     css`
-      opacity: ${({ colorScheme }) => (colorScheme === 'white' ? 0.5 : 0.3)};
+      opacity: 0.5;
     `}
 
   ${({ active }) =>
