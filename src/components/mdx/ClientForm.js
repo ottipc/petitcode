5d00590f07e9af3914a2a836f722d29963310ab3
Benedikt Rötsch <info@benedikt-roetsch.de>
2019-05-09 13:36:15 +0200

    update form framework

--- src/components/mdx/ClientForm.js
+++ src/components/mdx/ClientForm.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react'
 
-import { useForm, useField } from 'react-final-form-hooks'
+import { Form } from 'react-final-form'
+import { Field } from 'react-final-form-html5-validation'
 import { LocationContext } from '../../utils/Contexts'
 
 import styled, { css } from 'styled-components'
@@ -69,6 +70,11 @@ const Submit = styled.button`
     `}
 `
 
+const Error = styled.div`
+  opacity: 0.8;
+  font-size: 0.8em;
+`
+
 export default function ClientForm() {
   const {
     location: { pathname }
@@ -76,43 +82,86 @@ export default function ClientForm() {
   const onSubmit = () => {
     console.log('thanks')
   }
-  const { form, handleSubmit, pristine, submitting } = useForm({
-    onSubmit // the function to call with your form values upon valid submit
-    // validate // a record-level validation function to check all form values
-  })
-  const name = useField('name', form, (val) =>
-    val ? null : 'Please let us know your name'
-  )
-  const email = useField('email', form, (val) =>
-    val ? null : 'Please let us know your email address'
-  )
-  const message = useField('message', form, (val) =>
-    val ? null : 'Please let us know what u want to tell us'
-  )
   return (
     <Wrapper>
-      <form
-        onSubmit={handleSubmit}
-        data-netlify="true"
-        name="client-form"
-        action={`${pathname}success`}
-      >
-        <Input {...name.input} placeholder="Name" />
-        {name.meta.touched && name.meta.error && <span>{name.meta.error}</span>}
-        <Separator />
-        <Input {...email.input} placeholder="Email" />
-        {email.meta.touched && email.meta.error && (
-          <span>{email.meta.error}</span>
+      <Form
+        onSubmit={onSubmit}
+        render={({ handleSubmit, pristine, invalid }) => (
+          <form
+            onSubmit={handleSubmit}
+            data-netlify="true"
+            name="client-form"
+            action={`${pathname}success`}
+          >
+            <Field
+              name="name"
+              tooShort="Your full name would be great"
+              valueMissing="Tell us who u are"
+            >
+              {({ input, meta }) => (
+                <>
+                  <Input
+                    {...input}
+                    type="text"
+                    placeholder="Name"
+                    required
+                    minLength={3}
+                  />
+                  <Error>
+                    {meta.touched && meta.error && <span>{meta.error}</span>}
+                  </Error>
+                </>
+              )}
+            </Field>
+            <Separator />
+            <Field
+              name="email"
+              valueMissing="Tell us your email so we can contact you"
+              patternMismatch="Make sure your email is valid"
+            >
+              {({ input, meta }) => (
+                <>
+                  <Input
+                    {...input}
+                    type="text"
+                    placeholder="Email"
+                    required
+                    minLength={3}
+                    pattern=".+@.+"
+                  />
+                  <Error>
+                    {meta.touched && meta.error && <span>{meta.error}</span>}
+                  </Error>
+                </>
+              )}
+            </Field>
+            <Separator />
+            <Field
+              name="message"
+              tooShort="Tell us a little bit more"
+              valueMissing="What do you want to tell us?"
+            >
+              {({ input, meta }) => (
+                <>
+                  <TextArea
+                    {...input}
+                    type="text"
+                    placeholder="Message"
+                    required
+                    minLength={10}
+                  />
+                  <Error>
+                    {meta.touched && meta.error && <span>{meta.error}</span>}
+                  </Error>
+                </>
+              )}
+            </Field>
+            <Submit type="submit" disabled={pristine || invalid}>
+              Submit
+            </Submit>
+          </form>
         )}
-        <Separator />
-        <TextArea {...message.input} placeholder="Message" />
-        {message.meta.touched && message.meta.error && (
-          <span>{message.meta.error}</span>
-        )}
-        <Submit type="submit" disabled={pristine || submitting}>
-          Submit
-        </Submit>
-      </form>
+      />
     </Wrapper>
   )
 }
