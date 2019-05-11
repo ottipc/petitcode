e785d59fded0eead8b21ceaca4dd81aeb2d376a9
Benedikt Rötsch <info@benedikt-roetsch.de>
2019-05-11 03:11:21 +0200

    swap icons in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,4 @@
 import React, { useContext } from 'react'
-// import propTypes from 'prop-types'
 import styled, { css } from 'styled-components'
 import { Link } from 'gatsby'
 
@@ -145,17 +144,13 @@ export default function Header() {
         </LogoWrapper>
         <ContactWrapper show={!isScrolling}>
           <a className="nohover" href="mailto:hi@petitcode.de">
-            <TelephoneIcon />
+            <EmailIcon />
           </a>
           <a className="nohover" href="tel:+493064080338">
-            <EmailIcon />
+            <TelephoneIcon />
           </a>
         </ContactWrapper>
       </Grid>
     </Wrapper>
   )
 }
-
-// Header.propTypes = {
-//   colorScheme: propTypes.string.isRequired
-// }
