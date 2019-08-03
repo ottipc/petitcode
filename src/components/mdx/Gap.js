4208bece3c8627c4c238166068893b17e5c912ae
Benedikt Rötsch <opensource@axe312.dev>
2019-08-03 15:29:10 +0200

    fix Gap export

--- src/components/mdx/Gap.js
+++ src/components/mdx/Gap.js
@@ -1,9 +1,11 @@
 import styled, { css } from 'styled-components'
 
-export const Gap = styled.div`
+const Gap = styled.div`
   ${({ gap = 0, theme }) =>
     gap &&
     css`
       margin-top: ${theme.spacings[`s${gap}`]};
     `};
 `
+
+export default Gap
