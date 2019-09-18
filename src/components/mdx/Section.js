e487b6c39ffac152d04fef804db6a36a848feef7
Benedikt Rötsch <bene@Benedikts-MBP.fritz.box>
2019-09-18 13:08:36 +0200

    remove greyscale on images

--- src/components/mdx/Section.js
+++ src/components/mdx/Section.js
@@ -63,6 +63,7 @@ const Wrapper = styled.section`
       }
     `}
 
+    /*
     & img, svg {
       filter: grayscale(100%);
       transition: 1s filter linear, 0.5s opacity linear !important;
@@ -71,6 +72,7 @@ const Wrapper = styled.section`
         filter: grayscale(0%);
       }
     }
+    */
   }
 `
 
